b442b236b6ff9f1b44603c34e54012dbe7bdf8df
Fabio Lima Santos <flsusp@gmail.com>
2015-08-25 21:28:58 -0300

    Adding the Java Cookbook because the master branch have a fix not present on the last release related to ShellOut timeout

--- site-cookbooks/basement/attributes/default.rb
+++ site-cookbooks/basement/attributes/default.rb
@@ -1,6 +1,8 @@
 default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['ark_timeout'] = 7200
+default['java']['ark_download_timeout'] = 7200
 
 default['nodejs']['npm_packages'] = [
   { 'name' => 'gulp' },
